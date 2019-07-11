eaaea8a8a9e2a778aa2856d6a705556b4b1aa047
Davi Silva <davi.cunha.silva.ca@gmail.com>
2019-07-10 23:43:42 -0300

    .

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,28 +12,24 @@ class Counter extends Component {
         color: '#fff'
     }
 
+    renderTags() {
+        if (this.state.tags.length === 0) {
+            return <p>There are no tags!</p>;
+        }
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    }
+
     render() {
         return (
             <React.Fragment>
-                <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button style={{fontSize: 14}} className="btn btn-secondary btn-sm">Increment</button>
                 <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                    {this.renderTags()}
                 </ul>
             </React.Fragment>
         );
     }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? 'warning' : 'primary';
-        return classes;
-    }
 
-    formatCount() {
-        const { count } = this.state
-        return count === 0 ? 'Zero' : count;
-    }
 }
  
 export default Counter;
