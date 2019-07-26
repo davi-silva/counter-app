517a0a4c96686e013221d8bfe4e7010f7fc81162
Davi Silva <davi.cunha.silva.ca@gmail.com>
2019-07-26 17:43:55 -0300

    Updates

---
+++ precache-manifest.95945a5e6d248c359015d4c210c435b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e87d662ca2b021fa22dce1b38864b2",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "42615ec589e51d02d46d",
+    "url": "/counter-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9ab3b92eceb509a80a1a",
+    "url": "/counter-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "42615ec589e51d02d46d",
+    "url": "/counter-app/static/js/2.45ba6aa1.chunk.js"
+  },
+  {
+    "revision": "9ab3b92eceb509a80a1a",
+    "url": "/counter-app/static/js/main.66ca42ef.chunk.js"
+  },
+  {
+    "revision": "0f1f037b32656fd93884",
+    "url": "/counter-app/static/js/runtime~main.c63f0888.js"
+  }
+]);
